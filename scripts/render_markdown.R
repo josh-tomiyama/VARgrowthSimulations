66833029bab46f6b014dac5aef2f66ae21d1413a
Tomiyama, Joshua-Michael K <joshua-michael-tomiyama@uiowa.edu>
2023-04-16 23:36:35 -0500

    Batch script for hpc

---
+++ scripts/render_markdown.R
@@ -0,0 +1,20 @@
+library(optparse)
+library(markdown)
+option_list <- list(
+  make_option(c("-t", "--template"), type = "character",
+              default = "./Templates/BiasEvaluationTemplate.Rmd", help = "config file path",
+              metavar="character"),
+  make_option(c("-d", "--dir"), type = "character",
+              default = "./data/InterceptBiasSimulation/", help = "config file path",
+              metavar="character"),
+  make_option(c("-n", "--num"), type = "character",
+              default = "1", help = "config file path",
+              metavar="character")
+)
+
+
+opt_parser <- OptionParser(option_list = option_list)
+opt = parse_args(opt_parser)
+
+file.copy(opt$template, paste0(opt$dir, "Sim", opt$num, "/template.Rmd"))
+rmarkdown::render(paste0(opt$dir, "Sim", opt$num, "/template.Rmd"))
